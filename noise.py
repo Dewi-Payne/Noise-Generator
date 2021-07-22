7e5bddac6aab8e20d31cc4e690c16ff95961d8de
Dewi-Payne <dewitbpayne@gmail.com>
2021-07-22 22:08:49 +0100

    starting implementing

--- noise.py
+++ noise.py
@@ -1,9 +1,11 @@
-# A program that implements simplex noise
+# A script that implements 2D simplex noise
 # 2021, Dewi Payne
 
-# Credit to Stefan Gustavson for the paper explaining the concept, and Ken Perlin for originally creating it
+# Credit to Stefan Gustavson for the paper explaining the concept and sample java code,
+# and Ken Perlin for originally creating the algorithm
 # https://weber.itn.liu.se/~stegu/simplexnoise/simplexnoise.pdf
 import random
+import math
 
 
 def random_noise():
@@ -14,7 +16,26 @@ def random_noise():
 def simplex_noise(x, y):
     # returns a value of 2-dimension simplex noise at a given point (x, y)
 
-    pass
+    # Contributions from the three corners of our 2d simplex triangles
+    n0, n1, n2 = 0, 0, 0
+
+    # Skewing the input to determine what cell we're in in the simplex grid
+    skew_factor = (math.sqrt(3)-1)/2
+    skew_amount = (x+y)*skew_factor
+    # These give the cell we're in
+    i = math.floor(x+skew_amount)
+    j = math.floor(y+skew_amount)
+
+    # un-skews the cells back to our original input space
+    unskew_factor = (3-math.sqrt(3))/6
+    unskew_amount = (i+j) * unskew_factor
+    X0 = i - unskew_amount
+    Y0 = j - unskew_amount
+    x0 = x - X0
+    y0 = y - Y0
+
+    # Here we find what equilateral triangle we're in depending on if x or y is greater
+    i1, j1 = (1, 0) if x0 > y0 else (0, 1)
 
 
 if __name__ == '__main__':
